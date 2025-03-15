4bf55f5d726fda1fe7276841938f04180c17446d
Your Name <your-email@example.com>
2025-03-15 12:11:01 +0530

    Added material1 page

--- pages/mechanical/semester1.js
+++ pages/mechanical/semester1.js
@@ -6,11 +6,9 @@ export default function Semester1() {
       <h1>Semester 1</h1>
       <ul>
         <li>
-          {/* Example of linking to a material */}
           <Link href="/mechanical/semester1/material1">Material 1</Link>
         </li>
         <li>
-          {/* Example of linking to another material */}
           <Link href="/mechanical/semester1/material2">Material 2</Link>
         </li>
       </ul>
---
+++ pages/mechanical/semester1/material1.js
@@ -0,0 +1,11 @@
+export default function Material1() {
+  return (
+    <div>
+      <h1>Material 1</h1>
+      {/* Add your study material content here */}
+      <p>This is where you'll display your study material.</p>
+      {/* Example: Displaying a downloadable PDF */}
+      <a href="my-study-site\public\Practice problem 5.pdf" download>Download Practice problem 5</a>
+    </div>
+  );
+}
